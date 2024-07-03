2a1988657f54a7f029e3b63d20a70c72c892cbdc
IvanOmelchenkoIP <io25112002@gmail.com>
2024-07-03 13:38:25 +0300

    Add basic routing
    Add basic routing with base static recognition and update resources for
    testing purposes

--- src/server/server-files/http-server.js
+++ src/server/server-files/http-server.js
@@ -3,25 +3,71 @@
 import http from "http";
 import fs from "fs";
 
+const isRouteStatic = (route) => {
+  const staticRegex = /\.(html|css|js)$/;
+  return staticRegex.test(route);
+};
+
+const getExtention = (filepath) => filepath.split(".").pop();
+
 class HttpServer {
   #port;
   #staticRoot;
   #server;
+  #cache;
   constructor(port, staticRoot) {
     this.#port = port;
     this.#staticRoot = staticRoot;
+    this.#cache = new Map();
     this.#server = http.createServer((req, res) => {
       const route = req.url;
       if (route == "./favicon.ico") return;
-      if (route == "/") {
-        const staticPath = this.#staticRoot + "/html/home.html";
+      if (isRouteStatic(route)) {
+        const STATIC_CONTENT_TYPES = {
+          html: { "Content-type": "text/html" },
+          css: { "Content-type": "text/css" },
+          js: { "Content-type": "text/javascript" },
+        };
+        const data = this.#cache.get(route);
+        const extension = getExtention(route);
+        const contentType = STATIC_CONTENT_TYPES[extension];
+        if (data) {
+          res.writeHead(200, contentType);
+          res.write(data);
+          res.end();
+          return;
+        }
+        const staticPath = this.#staticRoot + route;
         fs.readFile(staticPath, "utf-8", (err, data) => {
           if (err) {
             res.writeHead(404, { "Content-type": "text/plain" });
             res.end();
             return;
           }
-          res.writeHead(200, { "Content-type": "text-html" });
+          this.#cache.set(route, data);
+          res.writeHead(200, contentType);
+          res.write(data);
+          res.end();
+        });
+      }
+      if (route == "/") {
+        const relativePath = "/html/home.html";
+        const data = this.#cache.get(relativePath);
+        if (data) {
+          res.writeHead(200, { "Content-type": "text/html" });
+          res.write(data);
+          res.end();
+          return;
+        }
+        const staticPath = this.#staticRoot + relativePath;
+        fs.readFile(staticPath, "utf-8", (err, data) => {
+          if (err) {
+            res.writeHead(404, { "Content-type": "text/plain" });
+            res.end();
+            return;
+          }
+          this.#cache.set(relativePath, data);
+          res.writeHead(200, { "Content-type": "text/html" });
           res.write(data);
           res.end();
         });
